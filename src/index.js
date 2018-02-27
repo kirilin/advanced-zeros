41881f5b4c5e06d9b75819ecac77563d9bd29d40
Yuri <yura.kirilin@gmail.com>
2018-02-27 18:19:24 +0300

    Task: advanced-zeros solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+  //Наибольший простой делитель  будет от 2 до 128 (256/2)  т.к. `2 <= base <= 256`
+  let smplNum = [127, 113, 109, 107, 103, 101, 97, 89, 83, 79, 73, 71, 67, 61, 59, 53, 47, 43, 41, 37, 31, 29, 23, 19, 17, 13, 11, 7, 5, 3, 2];
+  let divider = base;
+  let zeroCount = 0;
+
+  //находим наибольший простой делитель
+  for (let i = 0; i < smplNum.length; i++){
+    if(base % smplNum[i] === 0) {
+      divider = smplNum[i];
+      break;
+    }
+  }
+  
+  //проверяем кратность второго делителя к простому
+  let tmpBase = base;
+  let zeroDiv = 0;
+  while (tmpBase % divider === 0){ 
+    tmpBase /= divider;
+    zeroDiv++;
+  }
+  
+  while (number >= divider) {
+    number = Math.trunc(number / divider);
+    zeroCount = zeroCount + number;
+  }
+
+  return Math.trunc(zeroCount / zeroDiv);
 }
